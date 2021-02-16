018ea1e65dfa5a5e7b4f1d494d13324858c6b183
ahamdum01 <ahmad.um.2001@mail.ru>
2021-02-16 23:36:21 +0300

    планы изменились

--- Gui.py
+++ Gui.py
@@ -1,3 +1,23 @@
 from tkinter import *
 root = Tk()
 root.wm_state("zoomed")
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+canvas = Canvas(root, width=w, height=h)
+canvas.pack()
+oval1 = canvas.create_oval(0, 0, 100, 100, fill='red')
+oval2 = canvas.create_oval(100, 100, 200, 200, fill='black')
+rec = canvas.create_rectangle(200, 200, 300, 300, fill="blue")
+print(canvas.coords(oval1))
+def move(event, shape):
+    canvas.coords(shape, 
+        event.x + (canvas.coords(shape)[2] - canvas.coords(shape)[0])/2,
+        event.y + (canvas.coords(shape)[3] - canvas.coords(shape)[1])/2,
+        event.x - (canvas.coords(shape)[2] - canvas.coords(shape)[0])/2,
+        event.y - (canvas.coords(shape)[3] - canvas.coords(shape)[1])/2
+    )
+
+a = [oval1, oval2, rec]
+for i in a:
+    eval("canvas.tag_bind({}, '<B1-Motion>', lambda event: move(event, {}))".format(i, i))
+
+root.mainloop()
